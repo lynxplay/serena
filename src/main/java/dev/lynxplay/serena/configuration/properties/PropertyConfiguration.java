fd26e3eff5632356cfc912cfc2af75e9ce872fe7
Bjarne Koll <lynxplay101@gmail.com>
2020-12-21 19:37:42 +0100

    Implement a banned entity set to serena logic
    As some servers may only want specific entities to be picked up by
    serena, this commit creates a configurable blacklist that contains the
    namespaced ids of the entity types that may not be interacted with
    through serena logic.
    
    Resolves: #6

--- src/main/java/dev/lynxplay/serena/configuration/properties/PropertyConfiguration.java
+++ src/main/java/dev/lynxplay/serena/configuration/properties/PropertyConfiguration.java
@@ -1,6 +1,9 @@
 package dev.lynxplay.serena.configuration.properties;
 
+import org.bukkit.entity.EntityType;
+
 import java.time.Duration;
+import java.util.Set;
 
 /**
  * The {@link PropertyConfiguration} defines a configuration that contains the properties of this plugin
@@ -20,5 +23,14 @@ public interface PropertyConfiguration {
      * @return the double value
      */
     double throwVelocityMultiplier();
+
+    /**
+     * Provides a set containing all entity types that are banned from being picked up by serena.
+     * An entity type that is listed in the set cannot be picked up or thrown off the player by serena.
+     *
+     * @return the set instance. The set is mutable but is not backed by this configuration, hence modifications to the
+     *     set will not modify this configuration instance.
+     */
+    Set<EntityType> bannedEntityTypes();
 
 }
